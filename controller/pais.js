1ae1c242c8978434bb75802f497a5384128425e9
ElisaCalderon <1180748@usap.edu>
2023-07-27 10:59:38 -0600

    Creacion API pais

---
+++ controller/pais.js
@@ -0,0 +1,29 @@
+const conexion = require('../database/bd');
+//GUARDAR un REGISTRO
+exports.guardarpais = (req, res)=>{
+    const idpais = req.body.idpais;
+    const nombre = req.body.nombre;
+   
+    conexion.query('INSERT INTO PAIS SET ?',{idpais:idpais, nombre:nombre}, (error, results)=>{
+        if(error){
+            console.log(error);
+        }else{
+            //console.log(results);   
+            res.redirect('/pais');         
+        }
+});
+};
+
+//ACTUALIZAR un REGISTRO
+exports.actualizapais = (req, res)=>{
+    const idpais = req.body.idpais;
+    const nombre = req.body.nombre;
+    
+    conexion.query('UPDATE PAIS SET ? WHERE idpais =?', [{nombre:nombre},idpais ], (error, results)=>{
+        if(error){
+            console.log(error);
+        }else{           
+            res.redirect('/pais');         
+        }
+});
+};
--- router.js
+++ router.js
@@ -6,8 +6,57 @@ const conexion = require('./database/bd');
 router.get('/', (req, res)=>{ 
     res.render('home.ejs'); 
    })
-   router.get('/pais', (req, res)=>{ 
-    res.render('pais.ejs'); 
-   })
+
+   router.get('/get_PAIS', function(request, response, next){
+    var buscar_query = request.query.buscar_query;
+    var query = `
+    SELECT PAIS FROM PAIS WHERE PAIS LIKE '%${buscar_query}%'
+    LIMIT 1 `;
+    conexion.query(query, function(error, data){
+    response.json(data);
+    });
+    });
+router.get('/PAIS', (req, res)=>{     
+    conexion.query('SELECT * FROM PAIS',(error, results)=>{
+        if(error){
+            throw error;
+        } else {                       
+            res.render('../views/pais.ejs', {results:results});            
+        }   
+    })
+    
+
+})
+
+router.get('/crearpais', (req,res)=>{
+    res.render('../views/crearpais.ejs');
+})
+
+router.get('/deletepais/:idpais', (req, res) => {
+    const ccate = req.params.ccate;
+    conexion.query('DELETE FROM PAIS WHERE idpais = ?',[idpais], (error, results)=>{
+        if(error){
+            console.log(error);
+        }else{           
+            res.redirect('/pais');         
+        }
+    })
+});
+
+router.get('/PAIS/:idpais', (req,res)=>{    
+    const idpais = req.params.idpais;
+    conexion.query('SELECT * FROM PAIS WHERE idpais=?',[idpais] , (error, results) => {
+        if (error) {
+            throw error;
+        }else{            
+            res.render('../views/editarpais.ejs', {pais:results[0]});            
+        }        
+    });
+});
+
+const pais = require('./controller/pais');
+  router.post('/guardarpais', pais.guardarpais);
+  router.post('/actualizapais', pais.actualizapais);
+
 
    module.exports = router;
