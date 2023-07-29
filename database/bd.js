099d76ef583ac90e66dafde7871c66360e985db2
usuario <albertoguzman748@gmail.com>
2023-07-29 16:59:50 -0600

    L

--- database/bd.js
+++ database/bd.js
@@ -1,15 +1,15 @@
-const mysql = require('mysql');
-const conexion = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : 'rosaelisa123',
-    database : 'RESERVAV'  
-});
-conexion.connect((error)=>{
-    if (error) {
-      console.error('El error de conexión es: ' + error);
-      return;
-    }
-    console.log('¡Conectado a la Base de Datos!');
-  });
-module.exports = conexion;
+///*const mysql = require('mysql');
+//const conexion = mysql.createConnection({
+   // host     : 'localhost',
+  //  user     : 'root',
+  //  password : 'rosaelisa123',
+  //  database : 'RESERVAV'  
+//}//);
+//conexion.connect((error)=>{
+  //  if (error) {
+   //   console.error('El error de conexión es: ' + error);
+   //   return;
+   // }
+   // console.log('¡Conectado a la Base de Datos!');
+  //});
+//module.exports = conexion;
